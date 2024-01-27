8760ea1b24acfa2d1f6540f12bbcbc43f930846b
Zolo <39656359+zolodev@users.noreply.github.com>
2024-01-27 01:37:49 +0100

    updating skandia fetcher

--- fetchSkandiaData.py
+++ fetchSkandiaData.py
@@ -10,10 +10,33 @@ import os
 import json
 from seleniumwire import webdriver
 from haralyzer import HarParser, HarPage
-
+from datetime import datetime
 
 TITLE = "Skandia"
 
+
+def obj_dict(obj):
+    return obj.__dict__
+
+class FundData(object):
+    Name = ""
+    isin = ""
+    URL = ""
+    LastUpdated = ""
+    Day = 0.0
+    Week = 0.0
+    
+
+    def __init__(self, name, isin, url, lastUpdated, day, week):
+        self.Name = name
+        self.isin = isin
+        self.URL = url
+        self.LastUpdated = lastUpdated
+        self.Day = day
+        self.Week = week
+
+    def to_json(self): 
+        return json.dumps(self, indent = 4, default=lambda o: o.__dict__) 
 
 def write_HAR_file(filename=None, har_to_write=""):
     # Write the information to a new JSON file
@@ -85,7 +108,68 @@ def find_HAR_content_by_req(filename, part_to_find=""):
                 with open(filename+'_data.json', 'w') as f:
                     f.write(json.dumps(json.loads(res_content), indent=4, sort_keys=True))
 
-    
+
+def filter_skandia_json_data(filename="Skandia"):
+    skandia_data = ""
+    with open('/content/'+filename+'_data.json') as f:
+        skandia_data = json.load(f)
+
+    filtered_funds = []
+    for i in range(0, len(skandia_data)):
+        
+        # Check if the fund is in correct collection
+        if "Fondf\u00f6rs\u00e4kring" not in skandia_data[i]["collections"]:
+            continue
+
+    returnOneDay = float(skandia_data[i]["returnOneDay"]) if skandia_data[i]["returnOneDay"] is not None else 0
+    returnOneWeek = float(skandia_data[i]["returnOneWeek"]) if skandia_data[i]["returnOneWeek"] is not None else 0
+    returnOneMonth = float(skandia_data[i]["returnOneMonth"]) if skandia_data[i]["returnOneMonth"] is not None else 0
+    returnThreeMonths = float(skandia_data[i]["returnThreeMonths"]) if skandia_data[i]["returnThreeMonths"] is not None else 0
+    returnSixMonths = float(skandia_data[i]["returnSixMonths"]) if skandia_data[i]["returnSixMonths"] is not None else 0
+    returnThisYear = float(skandia_data[i]["returnThisYear"]) if skandia_data[i]["returnThisYear"] is not None else 0
+    returnOneYear = float(skandia_data[i]["returnOneYear"]) if skandia_data[i]["returnOneYear"] is not None else 0
+    returnThreeYears = float(skandia_data[i]["returnThreeYears"]) if skandia_data[i]["returnThreeYears"] is not None else 0
+    returnFiveYears = float(skandia_data[i]["returnFiveYears"]) if skandia_data[i]["returnFiveYears"] is not None else 0
+    returnTenYears = float(skandia_data[i]["returnTenYears"]) if skandia_data[i]["returnTenYears"] is not None else 0
+
+
+    # Check all values to be positive before adding the fund to the list
+    if( returnOneDay > 0 and
+        returnOneWeek > 0 and
+        returnOneMonth> 0 and
+        returnThreeMonths > 0 and
+        returnSixMonths > 0 and
+        returnThisYear > 0 and
+        returnOneYear > 0 and
+        returnThreeYears > 0 and
+        returnFiveYears > 0 and
+        returnTenYears > 0):
+            fund_name = skandia_data[i]["name"]
+            fund_isin = skandia_data[i]["isin"]
+            fund_currency = skandia_data[i]["currency"]
+            
+            fund_url_base = "https://www.skandia.se/spara-pension/satt-att-spara/spara-fonder/fondlista?collections=Fondf%C3%B6rs%C3%A4kring#/fund/details/[isin]/[currency]"
+            fund_url = fund_url_base.replace("[isin]", str(fund_isin))
+            fund_url = fund_url.replace("[currency]", str(fund_currency))
+            
+            # Get current date and time
+            now = datetime.now()
+            dt_string = now.strftime("%Y-%m-%d %H:%M:%S") # YYYY-MM-DD HH:MM:SS
+            
+            fundDataObj = {}
+            fundDataObj = FundData( name = fund_name,
+                                    isin = fund_isin,
+                                    url = fund_url,
+                                    lastUpdated = str(dt_string) + " (UTC)",
+                                    day = returnOneDay,
+                                    week = returnOneWeek)
+
+            filtered_funds.append(fundDataObj)
+
+    with open(filename+'_data_filtered.json', 'w') as f:
+        f.write(json.dumps(filtered_funds, indent=4, default=lambda o: o.__dict__))
+
+    return filtered_funds
 
 
 def run():
@@ -94,6 +178,8 @@ def run():
 
     find_HAR_content_by_req(TITLE, "/api/fundlist/desktop")
 
+    filter_skandia_json_data()
+    
     # Delete the HAR file because it could be large and unnecessary to have it in the repo
     os.remove("HAR_Skandia.har")
     
